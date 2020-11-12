69efc6a2e4a2b996ec6d94525528d0ffe6d37f28
Marcos de la Calle <marcos.dlcs@gmail.com>
2020-11-12 20:05:34 +0100

    Add more information to README. Basic project scaffolding for example 01. Improve .gitignore file

---
+++ examples/01-basic-comparison/spring-mvc-api/src/main/java/es/codeurjc/arpj/mvcapi/MvcApiApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.arpj.mvcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcApiApplication.class, args);
+	}
+
+}
