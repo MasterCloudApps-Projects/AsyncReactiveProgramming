9d8ca02fb52741aa3c94c84db2e7ad1fe88998f6
Marcos de la Calle <marcosDLCS@users.noreply.github.com>
2020-11-18 20:08:45 +0100

    Add new tests for example 00. Update presentation links (#14)

--- examples/00-reactor-operators/project-reactor-operators/src/test/java/es/codeurjc/arpj/FluxTest.java
+++ examples/00-reactor-operators/project-reactor-operators/src/test/java/es/codeurjc/arpj/FluxTest.java
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 class FluxTest {
 
     private static final Faker FAKER = new Faker();
 
     @Test
     @DisplayName("Test 01: Basic Flux")
-    void test() {
+    void basic_flux_test() {
 
-        final Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5);
+        System.out.println("Test 01: Basic Flux");
+        System.out.println("=======================================================================================\n");
+
+        final Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5).log();
 
         StepVerifier
                 .create(flux)
@@ -23,4 +29,47 @@ class FluxTest {
                 .verify();
     }
 
+    @Test
+    @DisplayName("Test 02: Flux from...")
+    void flux_from_test() {
+
+        System.out.println("Test 02: Flux from...");
+        System.out.println("=======================================================================================\n");
+
+        final String[] strings = {"one", "two", "three", "four", "five"};
+        final Flux<String> stringFlux = Flux.fromArray(strings).log();
+
+        StepVerifier
+                .create(stringFlux)
+                .expectNextCount(5)
+                .expectComplete()
+                .verify();
+
+        final List<String> cats = List.of(FAKER.cat().name(), FAKER.cat().name(), FAKER.cat().name());
+        final Flux<String> catFlux = Flux.fromIterable(cats).log();
+
+        StepVerifier
+                .create(catFlux)
+                .expectNextCount(3)
+                .expectComplete()
+                .verify();
+
+        final Flux<Integer> integerFlux = Flux.range(1, 10).log();
+
+        StepVerifier
+                .create(integerFlux)
+                .expectNextCount(10)
+                .expectComplete()
+                .verify();
+
+        final Stream<String> dragons = Stream.of(FAKER.gameOfThrones().dragon(), FAKER.gameOfThrones().dragon(),
+                FAKER.gameOfThrones().dragon());
+        final Flux<String> dragonFlux = Flux.fromStream(dragons).log();
+
+        StepVerifier
+                .create(dragonFlux)
+                .expectNextCount(3)
+                .expectComplete()
+                .verify();
+    }
 }
