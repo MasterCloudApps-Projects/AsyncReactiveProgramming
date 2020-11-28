185d99552dd3fe99e70ba53d3f9eff3d7feab455
Marcos de la Calle <marcos.dlcs@gmail.com>
2020-11-28 19:39:17 +0100

    Add new awesome master logo! READMEs cleanup. Example 05 RSocket scaffolding

---
+++ examples/05-rsocket/rsocket-service-one/src/main/java/es/codeurjc/arpj/rsocket_one/RsocketOneApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.arpj.rsocket_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketOneApplication.class, args);
+    }
+
+}
---
+++ examples/05-rsocket/rsocket-service-two/src/main/java/es/codeurjc/arpj/rsocket_two/RsocketTwoApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.arpj.rsocket_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketTwoApplication.class, args);
+	}
+
+}
