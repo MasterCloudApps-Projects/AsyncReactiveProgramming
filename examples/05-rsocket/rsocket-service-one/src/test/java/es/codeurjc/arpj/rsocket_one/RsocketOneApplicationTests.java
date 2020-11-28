8e81d29a22f221abd932cfa40d5c9ac6b826ccf0
Marcos de la Calle <marcosDLCS@users.noreply.github.com>
2020-11-28 19:40:46 +0100

    Add new awesome master logo! READMEs cleanup. Example 05 RSocket scaffolding (#25)

---
+++ examples/05-rsocket/rsocket-service-one/src/test/java/es/codeurjc/arpj/rsocket_one/RsocketOneApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.arpj.rsocket_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ examples/05-rsocket/rsocket-service-two/src/test/java/es/codeurjc/arpj/rsocket_two/RsocketTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.arpj.rsocket_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
