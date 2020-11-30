da72fd62e5b785e4e9664f28a82a09bf251555ac
Marcos de la Calle <marcos.dlcs@gmail.com>
2020-11-30 20:52:50 +0100

    Add more examples (TestPublisher, PublisherProbe). Update presentation

---
+++ examples/00-reactor-operators/project-reactor-operators/src/test/java/es/codeurjc/arpj/PublisherProbeTest.java
@@ -0,0 +1,46 @@
+package es.codeurjc.arpj;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.PublisherProbe;
+
+class PublisherProbeTest {
+
+    private static final Faker FAKER = new Faker();
+
+    @Test
+    @DisplayName("Test 01: Publisher Probe. Basic")
+    void basic_publisher_probe_1() {
+
+        final Mono<String> myDog = myComplexOperation(Mono.just(FAKER.dog().name()),
+                Mono.just(FAKER.animal().name())).log();
+
+        StepVerifier.create(myDog)
+                .expectNextCount(1)
+                .expectComplete()
+                .verify();
+
+        PublisherProbe<String> probe = PublisherProbe.of(Mono.just(FAKER.dog().name()));
+
+        final Mono<String> myDog2 = myComplexOperation(
+                Mono.empty(),
+                probe.mono())
+                .log();
+
+        StepVerifier.create(myDog2)
+                .expectNextCount(1)
+                .expectComplete()
+                .verify();
+
+        probe.assertWasSubscribed();
+        probe.assertWasRequested();
+        probe.assertWasNotCancelled();
+    }
+
+    private Mono<String> myComplexOperation(Mono<String> data, Mono<String> defaultValue) {
+        return data.switchIfEmpty(defaultValue);
+    }
+}
