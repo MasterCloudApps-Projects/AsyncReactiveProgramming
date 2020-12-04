d155f824455255e119c32547b23921a3fe34e1ed
Marcos de la Calle <marcos.dlcs@gmail.com>
2020-12-04 19:54:35 +0100

    Initial Project Loom example and README. Updated presentation

---
+++ examples/06-project-loom/loom/src/test/java/es/codeurjc/arpj/HelloLoomTest.java
@@ -0,0 +1,13 @@
+package es.codeurjc.arpj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloLoomTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertThat(Boolean.TRUE).isTrue();
+    }
+}
