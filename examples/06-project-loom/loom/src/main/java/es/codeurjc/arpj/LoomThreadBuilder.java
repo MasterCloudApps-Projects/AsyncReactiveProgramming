f1554eaaa6b1576c89454790ad610d3e2c460daf
Marcos de la Calle <marcosDLCS@users.noreply.github.com>
2020-12-06 19:31:32 +0100

    Project Loom examples and updated presentation (#35)

---
+++ examples/06-project-loom/loom/src/main/java/es/codeurjc/arpj/LoomThreadBuilder.java
@@ -0,0 +1,20 @@
+package es.codeurjc.arpj;
+
+public class LoomThreadBuilder {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Hello @ " + Thread.currentThread().getName());
+
+        Thread myVt = Thread.builder()
+                .virtual()
+                .name("my-virtual-thread")
+                .task(() -> System.out.println("Hello Loom! @ "
+                        + Thread.currentThread().getName()))
+                .build();
+        myVt.start();
+
+        Thread.sleep(1000);
+        System.out.println("Goodbye @ " + Thread.currentThread().getName());
+    }
+}
